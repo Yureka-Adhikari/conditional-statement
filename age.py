8750651ac241c4a1425c0f20c3fb583dea1aafe0
Yureka Adhikari <adhikariyureka@gmail.com>
2024-11-20 17:22:02 +0545

    YAYYYYTYYY

---
+++ age.py
@@ -0,0 +1,10 @@
+age= int(input ( "Enter age: "))
+
+if age>0:
+    print ("You are alive, well done!")
+    if age > 18:
+        print ("You can get a drivers Licence")
+        if age >80:
+            print("Why are you trying to drive?")
+else:
+    print ("You arent alive yet. Become alive first!")
---
+++ odd even.py
@@ -0,0 +1,6 @@
+num = int(input ("Enter a number: "))
+
+if num % 2 ==0:
+    print(f"{num} is an EVEN NUMBERRR")
+else:
+    print(f"{num} is an ODD NUMBERRR")
